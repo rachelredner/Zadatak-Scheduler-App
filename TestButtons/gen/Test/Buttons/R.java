c498c21039fdb546f1127f6375d4d2fc84cb85c9
Rachel.Redner <rachelshaeredner@gmail.com>
2012-09-22 16:56:47 -0400

    Mega and mini buttons now have associated actions

--- TestButtons/gen/Test/Buttons/R.java
+++ TestButtons/gen/Test/Buttons/R.java
@@ -15,7 +15,14 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int editor=0x7f070001;
+        public static final int id8=0x7f070006;
+        public static final int id9=0x7f070005;
+        public static final int label1=0x7f070000;
+        public static final int mega=0x7f070003;
+        public static final int menu_settings=0x7f070007;
+        public static final int ok=0x7f070002;
+        public static final int seekBar1=0x7f070004;
     }
     public static final class layout {
         public static final int activity_test_buttons=0x7f030000;
--- TestButtons/src/Test/Buttons/TestButtons.java
+++ TestButtons/src/Test/Buttons/TestButtons.java
@@ -1,20 +1,110 @@
 package Test.Buttons;
 
-import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.Toast;
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class TestButtons extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_buttons);
-    }
+public class TestButtons extends Activity implements OnClickListener, OnLongClickListener {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_test_buttons, menu);
-        return true;
-    }
+	
+
+
+
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.activity_test_buttons);
+			
+			Button button = (Button)findViewById(R.id.mega);
+			button.setOnClickListener(this);
+			button.setOnLongClickListener(this);
+			Button contextbutton = (Button)findViewById(R.id.ok);
+			registerForContextMenu(contextbutton);
+		}
+
+		@Override
+		public boolean onCreateOptionsMenu(Menu menu) {
+			getMenuInflater().inflate(R.menu.activity_test_buttons, menu);
+			return true;
+		}
+		
+		@Override
+		public void onCreateContextMenu (ContextMenu menu, View view, ContextMenuInfo menuInfo)
+		{
+			super.onCreateContextMenu(menu, view, menuInfo);
+			menu.setHeaderTitle("An awesome menu");
+			menu.add(0, view.getId(), 0, "Choice 1");
+			menu.add(0, view.getId(), 0, "Choice 2");
+			
+		}
+		
+		
+		public boolean onContextItemSelected (MenuItem item)
+		{
+			if (item.getTitle() == "Choice 1")
+			{
+				contextFunction1(item.getItemId());
+			}
+			else if (item.getTitle() == "Choice 2")
+			{
+				contextFunction2(item.getItemId());
+			}
+			else
+				return false;
+			return true;
+		}
+		
+		
+		public void contextFunction1 (int id)
+		{
+			Toast.makeText(this, "You chose option 1", Toast.LENGTH_LONG).show();
+		}
+		
+		public void contextFunction2 (int id)
+		{
+			Toast.makeText(this, "You chose option 2", Toast.LENGTH_LONG).show();
+		}
+			
+		
+		public boolean onKeyDown(int keyCode, KeyEvent event)
+		{
+			if (keyCode == KeyEvent.KEYCODE_MENU)
+			{
+				TextView text = (TextView)findViewById(R.id.id8);
+				text.setText ("Menu Key Pressed!");
+				return true;
+			}
+			return false;
+		}
+	
+	
+
+	public void onClick(View v) {
+		TextView text = (TextView)findViewById(R.id.id9);
+		if (text.getText().toString().equals("MEGA BUTTON PRESSED!"))
+		{
+			text.setText(" ");
+		}
+		else
+			text.setText("MEGA BUTTON PRESSED!");
+		
+	}
+
+	public boolean onLongClick(View arg0) {
+		TextView text = (TextView)findViewById(R.id.id9);
+		text.setText("MEGA BUTTON HELD \n OH SO GENTLY ");	
+		return true;
+	}
 }
