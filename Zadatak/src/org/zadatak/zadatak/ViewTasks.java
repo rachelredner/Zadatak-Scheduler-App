b6a5f764171a2fafe44d88eb80383fa4942f56a6
Asher Glick <asher.glick@gmail.com>
2012-10-06 19:11:09 -0400

    created a sample list for the list activity

--- Zadatak/src/org/zadatak/zadatak/ViewTasks.java
+++ Zadatak/src/org/zadatak/zadatak/ViewTasks.java
@@ -2,14 +2,32 @@ package org.zadatak.zadatak;
 
 import android.os.Bundle;
 import android.app.ListActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 public class ViewTasks extends ListActivity {
-
+	
+	// This is the Adapter being used to display the list's data
+    SimpleCursorAdapter mAdapter;
+	
+	
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_tasks);
+    protected void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+            "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+            "Linux", "OS/2" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+            android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
     }
 
     @Override
