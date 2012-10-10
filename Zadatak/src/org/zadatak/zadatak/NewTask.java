a0d407e86effe0acb128f89dd83a97bc37ace0e4
Asher Glick <asher.glick@gmail.com>
2012-10-10 11:15:46 -0400

    the save button now closes the newtask activity

--- Zadatak/src/org/zadatak/zadatak/NewTask.java
+++ Zadatak/src/org/zadatak/zadatak/NewTask.java
@@ -3,6 +3,9 @@ package org.zadatak.zadatak;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class NewTask extends Activity {
 
@@ -10,11 +13,20 @@ public class NewTask extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+        
+        
+        Button save = (Button) findViewById(R.id.save);
+		save.setOnClickListener(new OnClickListener() { public void onClick(View v) { saveTask();} } );
+        
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_new_task, menu);
         return true;
+    }
+    
+    private void saveTask(){
+    	finish();
     }
 }
