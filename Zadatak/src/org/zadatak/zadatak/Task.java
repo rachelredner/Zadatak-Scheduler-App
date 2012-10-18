7e4d36961245038b7aed1b3e97d846b1ce7c0a2e
Asher Glick <asher.glick@gmail.com>
2012-10-18 02:51:09 -0400

    Created functions to set and get elements directly from the ui

--- Zadatak/src/org/zadatak/zadatak/Task.java
+++ Zadatak/src/org/zadatak/zadatak/Task.java
@@ -3,6 +3,10 @@ package org.zadatak.zadatak;
 import java.util.HashMap;
 import java.util.Map;
 
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
 public class Task {
 	
 	enum Attributes {
@@ -16,4 +20,38 @@ public class Task {
 	
 	public void set(Attributes attribute,String value) {values.put(attribute, value);}
 	public String get (Attributes attribute) {return values.get(attribute);}
+	
+	// Now for some getters and setters that are used with the UI elements
+	/*
+	 * Set from edittext
+	 */
+	public void set(Attributes attribute, EditText text) {
+		set(attribute,text.getText().toString());
+	}
+	public void get(Attributes attribute, EditText text) {
+		String savedText = get(attribute);
+		text.setText(savedText);
+	}
+	// DATETIME
+	public void set(Attributes attribute, DatePicker datePicker) {
+		String date = datePicker.getDayOfMonth() + "/" + datePicker.getMonth() + "/" + datePicker.getYear();
+		set(attribute, date);
+	}
+	public void get(Attributes attribute, DatePicker datePicker) {
+		String alldates = get(attribute);
+		String[] dates = alldates.split("/");
+		int year = Integer.getInteger(dates[2]);
+		int month = Integer.getInteger(dates[1]);
+		int day = Integer.getInteger(dates[0]);
+		datePicker.init(year, month, day, null);
+	}
+	// Checkbox
+	public void set (Attributes attribute, CheckBox checkBox){
+		set(attribute,checkBox.isChecked() ? "TRUE" : "FALSE");
+	}
+	public void get (Attributes attribute, CheckBox checkBox){
+		boolean checked = get(attribute) == "TRUE";
+		checkBox.setChecked(checked);
+	}
+	// Slider Bar
 }
