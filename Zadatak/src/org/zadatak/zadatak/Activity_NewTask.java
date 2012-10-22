1d5164b5db1dce2c887de1f5de723cea5b011304
Asher Glick <asher.glick@gmail.com>
2012-10-22 14:22:34 -0400

    added comments to the functions of the newtask activity

--- Zadatak/src/org/zadatak/zadatak/Activity_NewTask.java
+++ Zadatak/src/org/zadatak/zadatak/Activity_NewTask.java
@@ -23,11 +23,15 @@ public class Activity_NewTask extends Activity {
 	long id;
 	boolean editmode = false;
 	
+	/********************************** ON CREATE *********************************\
+	| This function finds the objects on the activites view and sets them to the   |
+	| global variables. It also checks to see if any data has been passed to it    |
+	| in the bundle, if data exists then it extracts the data and fills out the    |
+	| view to match the data passed                                                |
+	\******************************************************************************/
     @Override
     public void onCreate(Bundle savedInstanceState) {
        	super.onCreate(savedInstanceState);
-        
-        
         
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_new_task);
@@ -54,24 +58,30 @@ public class Activity_NewTask extends Activity {
         	editmode = true;
         }   
     	
-    	
-        
         Button save = (Button) findViewById(R.id.save);
 		save.setOnClickListener(new OnClickListener() { public void onClick(View v) { saveTask();} } );
         
     }
 
+    /*************************** ON CREATE OPTIONS MENU ***************************\
+    | This function creates the options menu at the bottom of the screen, it       |
+    | probably can be removed due to the options menu not currently having any     |
+    | task                                                                         |
+    \******************************************************************************/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_new_task, menu);
         return true;
     }
     
+    /********************************** SAVE TASK *********************************\
+    | This function will save a task to the databsae. If the activity is editing   |
+    | a task that was passed to it then the activity is saved over the old         |
+    | activity. If the activity is not editing a task then it will save the task   |
+    | as a new task in the database                                                |
+    \******************************************************************************/
     private void saveTask(){
     	ZadatakApp app = (ZadatakApp)getApplicationContext();
-    	
-    	
-    	
     	
     	Task task = new Task();
     	task.set(Task.Attributes.Name, nameText);
@@ -79,14 +89,12 @@ public class Activity_NewTask extends Activity {
     	task.set(Task.Attributes.Hours, lengthText);
     	task.set(Task.Attributes.Progress, progressBar);
     	task.set(Task.Attributes.Priority, priorityBox);
-
     	
-    	if (editmode) {
-    		app.dbman.editTask(id, task);
-    	}
-    	else {
-    		app.dbman.insertTask(task);
-    	}
+    	// Save it to the database either as a new task or over an old task
+    	if (editmode) { app.dbman.editTask(id, task); }
+    	else { app.dbman.insertTask(task); }
+    	
+    	// Quit the activity
     	finish();
     }
 }
