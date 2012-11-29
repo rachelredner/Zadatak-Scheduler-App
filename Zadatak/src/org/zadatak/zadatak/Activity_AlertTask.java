56682eeceb4d0161003ef080104c08fb10ddd2c0
Asher Glick <asher.glick@gmail.com>
2012-11-29 14:09:11 -0500

    added sounds to the alert menu

--- Zadatak/src/org/zadatak/zadatak/Activity_AlertTask.java
+++ Zadatak/src/org/zadatak/zadatak/Activity_AlertTask.java
@@ -1,9 +1,14 @@
 package org.zadatak.zadatak;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.view.Menu;
 import android.view.View;
@@ -26,14 +31,27 @@ public class Activity_AlertTask extends Activity {
         						  WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
         						  WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
         
-        //
-        Button beginTask = (Button)findViewById(R.id.beginTask);
-        beginTask.setOnClickListener(new OnClickListener(){ public void onClick(View v){ beginTask(); } } );
-		
-		Button postponeTask = (Button)findViewById(R.id.postponeTask);
-		postponeTask.setOnClickListener(new OnClickListener(){ public void onClick(View v){ postponeTask(); } } );
-		
-		playloop();
+        
+//      Button beginTask = (Button)findViewById(R.id.beginTask);
+//      beginTask.setOnClickListener(new OnClickListener(){ public void onClick(View v){ beginTask(); } } );
+//		
+//		Button postponeTask = (Button)findViewById(R.id.postponeTask);
+//		postponeTask.setOnClickListener(new OnClickListener(){ public void onClick(View v){ postponeTask(); } } );
+//		
+//		playloop();
+        
+        Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if(alert == null){
+            // alert is null, using backup
+            alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            if(alert == null){  // I can't see this ever being null (as always have a default notification) but just incase
+                // alert backup is null, using 2nd backup
+                alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);               
+            }
+        }
+        //Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), alert);
+        r.play();
 		
     }
     
