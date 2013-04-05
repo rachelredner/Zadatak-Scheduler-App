67d9fd6399e287eea1ca7d2ae2b6b343673bcafb
Asher Glick <asher.glick@gmail.com>
2013-04-04 23:27:58 -0400

    Added two more tables to the database
    The Tasks table was left the same, but the variable TABLE_NAME was changed to
    TASK_TABLE_NAME to avoid confusion with the other two tables
    
    A table containing the user settings was added in order to store the user
    profile setting for the application
    
    A table containing the schedule of event day by day was added so that the app
    can maintain a list of the tasks it has scheduled if it needs to display them
    or modify them without having to hit the calendar / internet

--- Zadatak/src/org/zadatak/zadatak/DatabaseHelper.java
+++ Zadatak/src/org/zadatak/zadatak/DatabaseHelper.java
@@ -9,8 +9,10 @@ import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 	
-	// TABLE NAME
-	public static final String TABLE_NAME = "tasks";
+	// TABLE NAMES
+	public static final String TASK_TABLE_NAME = "tasks";
+	public static final String SETTING_TABLE_NAME = "settings";
+	public static final String SCHEDULE_TABLE_NAME = "schedules";
 	
 	// VERSION NUMBER AND DATABASE NAME
 	private static final String DATABASE_NAME = "zadatak.db";
@@ -27,19 +29,30 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	}
 	
 	/********************************** ON CREATE *********************************\
-	| On Create creates a new sql table to contain all of the tasks for Zadatak    |
+	| This function is run when the application is started it is in charge of      |
+	| setting up the databases that will be used for the app. Three databases are  |
+	| created for this application. The database of tasks that must be             |
+	| accomplished, the database of user defined settings, and the database of     |
+	| scheduled tasks used by the scheduler class to persist data without having   |
+	| to recalculate it.                                                           |
 	\******************************************************************************/	
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		
+		// Create Tasks Database
 		Attributes[] rows = Task.Attributes.values();
-		String databasecreate = "create table " + TABLE_NAME + "("
+		String databasecreate = "create table " + TASK_TABLE_NAME + "("
 				+ ID_COLUMN_NAME +" integer primary key autoincrement";
 		for (int i = 0 ; i < rows.length; i++) {
 			databasecreate += "," + rows[i].toString() + " text";
 		}
 		databasecreate += ");";
 		db.execSQL(databasecreate);
+		
+		// Create Setting Database
+		db.execSQL("create table " + SETTING_TABLE_NAME +"("+ID_COLUMN_NAME+" integer primary key autoincrement, settingname text, settingvalue text);");
+		
+		// Create the Schedule database
+		db.execSQL("create table " + SCHEDULE_TABLE_NAME +"("+ID_COLUMN_NAME+" integer primary key autoincrement, day text, tasklist text);");
 	}
 	
 	/********************************* ON UPGRADE *********************************\
@@ -51,7 +64,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		Log.w(DatabaseHelper.class.getName(), "Upgrading database from version " + oldVersion + " to " + newVersion + ", which will destroy all old data");
-		db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+		db.execSQL("DROP TABLE IF EXISTS " + TASK_TABLE_NAME);
+		db.execSQL("DROP TABLE IF EXISTS " + SETTING_TABLE_NAME);
+		db.execSQL("DROP TABLE IF EXISTS " + SCHEDULE_TABLE_NAME);
 		onCreate(db);
 	}
 	
