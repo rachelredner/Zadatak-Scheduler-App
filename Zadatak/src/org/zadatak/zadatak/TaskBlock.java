47afe75e468862f6f994110db3f5d69d8b68ad17
Asher Glick <asher.glick@gmail.com>
2013-04-17 17:59:32 -0400

    added constructors to the task block class to allow it to be created quickly

--- Zadatak/src/org/zadatak/zadatak/TaskBlock.java
+++ Zadatak/src/org/zadatak/zadatak/TaskBlock.java
@@ -5,6 +5,22 @@ public class TaskBlock {
 	public Integer endTime;
 	public Task task;
 	
+	// A constructor to create a new task block with all the elements
+	public TaskBlock(Integer _startTime, Integer _endTime, Task _task) {
+		startTime = _startTime;
+		endTime = _endTime;
+		task = _task;
+	}
+	
+	// A constructor to create a new task block with only the hashable element
+	// used for lookups of this object in a hash table where the end time and task
+	// are trying to be determined
+	public TaskBlock(Integer _startTime) {
+		startTime = _startTime;
+	}
+	
+	
+	// A hashing function to allow lookup of the taskblock with only the start time
 	@Override
 	public int hashCode() {
 		int hash = startTime.hashCode();
