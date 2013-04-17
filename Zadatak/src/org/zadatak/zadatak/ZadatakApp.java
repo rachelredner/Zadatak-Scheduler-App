ec8f95675096a9f824af5d8db4d00aef526e768e
Nick Timakondu <nick.timakondu@gmail.com>
2013-04-17 13:58:54 -0400

    Adding nonGreedyScheduling function

--- Zadatak/src/org/zadatak/zadatak/ZadatakApp.java
+++ Zadatak/src/org/zadatak/zadatak/ZadatakApp.java
@@ -1,16 +1,23 @@
 package org.zadatak.zadatak;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Queue;
-
-import org.zadatak.zadatak.Task.Attributes;
+import java.util.Set;
+import java.util.TimeZone;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import android.app.Application;
+import android.util.Log;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioGroup;
@@ -171,6 +178,118 @@ public class ZadatakApp extends Application {
 	
 	public void postpone(int index) {
 		toaster("Unimplemented");
+	}
+	
+	/**
+	 * Allows sorting through Tasks by due date then by priority
+	 */
+	static Comparator<Task> DATE_ORDER = new Comparator<Task>() {
+		public int compare(Task t1, Task t2) {
+			Calendar c1, c2;
+			c1 = c2 = Calendar.getInstance((TimeZone.getTimeZone("UTC")));
+			SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy",
+					Locale.ENGLISH);
+			try {
+				c1.setTime(df.parse(t1.get(Task.Attributes.Duedate)));
+				c2.setTime(df.parse(t2.get(Task.Attributes.Duedate)));
+				
+			} catch (java.text.ParseException e) {
+				Log.e("ParseException", e.getMessage());
+				e.printStackTrace();
+			}
+			// Due date is equal
+			if (c1.compareTo(c2) == 0) {
+				Integer t1priority = t1.get(Task.Attributes.Priority)=="TRUE"?1:0;
+				Integer t2priority = t2.get(Task.Attributes.Priority)=="TRUE"?1:0;
+				return t1priority.compareTo(t2priority);
+
+			} else
+				return c1.compareTo(c2);
+		}
+
+	};
+	
+	public void nonGreedySchedule(int daysToSchedule, int currentDay, List<Task> tasks, List<Map<Integer, Integer>> busyTime){
+		
+		// Organize the list of tasks by priority, then by due date
+		Queue<Task> sortedTasks = new ConcurrentLinkedQueue<Task>();
+		List<Task> listCopy = new LinkedList<Task>(tasks);
+		
+		Collections.sort(listCopy, DATE_ORDER);
+		
+		// Put tasks into queue
+		for(Task t: listCopy)
+			sortedTasks.add(t);
+		
+		while(!sortedTasks.isEmpty()){
+			//Get Task at the top of the queue
+			Task currentTask = sortedTasks.poll();
+			
+			//Queue is empty
+			if(currentTask == null)
+				continue;
+			
+			// For the next daystoSchedule, find the day with the most amount of free time
+			int day = nextFreeDayProcrastinate(daysToSchedule, busyTime);
+			// Place an hour block of this task into this day
+			
+			
+		}
+		
+	}
+	
+	/**
+	 * Given the number of days to Schedule, return the furthest day with the most free time
+	 * @param daysToSchedule number of days from today to schedule
+	 * @param busyTime list of busy times per day
+	 * @return furthest day with most free time
+	 */
+	private int nextFreeDayProcrastinate(int daysToSchedule, List<Map<Integer, Integer>> busyTime){
+		int[] freetimes = new int[daysToSchedule];
+		
+		//Get free times for each day
+		for(int i = 0; i < daysToSchedule; i++){
+			Map<Integer, Integer> currentDay = busyTime.get(i);
+			int freeTime = 1440;
+			
+			for(Integer startTime : currentDay.keySet()){
+				freeTime -= (currentDay.get(startTime) - startTime);
+			}
+			
+			freetimes[i] = freeTime;
+		}
+		
+		int max = freetimes[0];
+		int maxElem = 0;
+		for(int j = freetimes.length -1 ; j > -1; j--){
+			if(freetimes[j] > max)
+				maxElem = j;
+		}
+		
+		return maxElem;
+	}
+	
+	/**
+	 * Find the next hour of free time on this day
+	 * @param startingHour hour to start looking at
+	 * @param busyDay this day's busy times
+	 * @return integer with the next free hour
+	 */
+	private int nextFreeHour(int startingHour, Map<Integer, Integer> busyDay){
+		Set<Integer> keys = busyDay.keySet();
+		int start = 0;
+		Iterator<Integer> keyIterator = keys.iterator();
+		while(keyIterator.hasNext()){
+			int key = keyIterator.next();
+			if(key <= start){
+				if(busyDay.get(key) > 60){
+					
+				}
+					//Do Stuff
+			}
+			// Not too sure where I'm going with this
+		}
+		return -1;
 	}
 	
 	
